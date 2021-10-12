67d16ba1e4cfee446114502f8e467ef38aeaf694
Paul Frazier <81392028+PxFrazier@users.noreply.github.com>
2021-10-12 18:04:35 -0400

    Add files via upload
    The index.ejs file is supposed to be in a views folder.  Make sure to run 'npm i express' before use.  I also used nodemon, so the package.json file contains "devStart": "nodemon server.js"

---
+++ server.js
@@ -0,0 +1,29 @@
+const fs = require('fs');
+const readLine = require('readline');
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+const list = [];
+
+const rd = readLine.createInterface({
+    input: fs.createReadStream('words.txt'),
+    output: process.stdout,
+    console: false
+});
+
+rd.on('line', (line)=>{
+    let original = String(line);
+    line = line.split('');
+    line = line.reverse().join('');
+    if(original == line) list.push(line);
+});
+
+app.set("view engine", "ejs");
+
+app.get('/', (req, res)=>{
+    res.render('index', {text: list});
+});
+
+app.listen(PORT, ()=>{
+    console.log(`Server started on port: ${PORT}`);
+});
